ed64d302ce80402139e946e84a443f70d6ae34fa
dudexior <dawidson1996@gmail.com>
2020-10-28 16:17:15 +0100

    dodano bootstrap i header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styles: [`
+    h1{
+      color: red;
+    }
+    `
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  appTitle = "Auction Portal!"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
