31d33b053511d6d73deae69a86695c6b241463e7
dudexior <dawidson1996@gmail.com>
2020-10-29 11:59:18 +0100

    inicjalizacja actions module i auctions page

---
+++ src/app/auction/auction.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuctionRoutingModule } from './auction-routing.module';
+import { AuctionsPageComponent } from './auctions-page/auctions-page.component';
+
+
+@NgModule({
+  declarations: [AuctionsPageComponent],
+  imports: [
+    CommonModule,
+    AuctionRoutingModule
+  ]
+})
+export class AuctionModule { }
---
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auctions-page',
+  template: `
+    <p>
+      auctions-page works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AuctionsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
