e552ac1634e030269411edf8f5761a2609bed3e4
dudexior <dawidson1996@gmail.com>
2020-10-29 13:56:12 +0100

    App title jest teraz inputem w komponencie headera

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  appTitle = 'Auction Portal!';
+  @Input() appTitle = 'Auction Portal!';
 
   constructor() { }
 
