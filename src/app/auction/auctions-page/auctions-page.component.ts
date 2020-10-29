10f3dc6a4b08cea493c93971695fab06b8eea5e6
dudexior <dawidson1996@gmail.com>
2020-10-29 15:18:30 +0100

    przeniesiono aukcje do serwisu

--- src/app/auction/auctions-page/auctions-page.component.ts
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Auction } from '../auction';
+import { AuctionsService } from '../auctions.service';
 
 @Component({
   selector: 'app-auctions-page',
@@ -18,27 +19,12 @@ import { Auction } from '../auction';
 })
 export class AuctionsPageComponent implements OnInit {
 
-  auctions: Auction[] = [
-    {
-      title: 'Kawiarka',
-      price: 60,
-      imgUrl: 'https://picsum.photos/id/464/200/200'
-    },
-    {
-      title: 'Garnek',
-      price: 100,
-      imgUrl: 'https://picsum.photos/id/123/200/200',
-      description: 'Voluptate veniam occaecat Lorem et eu Lorem. Non eiusmod enim cupidatat mollit. Enim duis non nostrud ex adipisicing nostrud eiusmod qui excepteur adipisicing quis in. Exercitation ipsum ad sint commodo incididunt anim ullamco exercitation.'
-    },
-    {
-      title: 'Czajnik',
-      price: 80,
-      imgUrl: 'https://picsum.photos/id/111/200/200'
-    }
-  ];
-  constructor() { }
+  auctions: Auction[] = [];
+
+  constructor(private auctionsService: AuctionsService) { }
 
   ngOnInit(): void {
+    this.auctions = this.auctionsService.getAuctions();
   }
 
 }
