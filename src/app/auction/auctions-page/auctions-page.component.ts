5ec9691422a4861025e828f9bed1c4fac432c392
dudexior <dawidson1996@gmail.com>
2020-10-29 14:03:46 +0100

    dodano więcej aukcji

--- src/app/auction/auctions-page/auctions-page.component.ts
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -22,13 +22,18 @@ export class AuctionsPageComponent implements OnInit {
     {
       title: 'Kawiarka',
       price: 60,
-      imgUrl: 'https://picsum.photos/id/36/200/200'
+      imgUrl: 'https://picsum.photos/id/464/200/200'
     },
     {
-      title: 'Kawiarka z opisem',
-      price: 60,
-      imgUrl: 'https://picsum.photos/id/36/200/200',
+      title: 'Garnek',
+      price: 100,
+      imgUrl: 'https://picsum.photos/id/123/200/200',
       description: 'Voluptate veniam occaecat Lorem et eu Lorem. Non eiusmod enim cupidatat mollit. Enim duis non nostrud ex adipisicing nostrud eiusmod qui excepteur adipisicing quis in. Exercitation ipsum ad sint commodo incididunt anim ullamco exercitation.'
+    },
+    {
+      title: 'Czajnik',
+      price: 80,
+      imgUrl: 'https://picsum.photos/id/111/200/200'
     }
   ]
   constructor() { }
