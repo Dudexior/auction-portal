ecfe1803650b1306a770cd2661dc1c4aebd81917
dudexior <dawidson1996@gmail.com>
2020-10-30 09:21:08 +0100

    dodano moduł advice i advices-page

---
+++ src/app/advice/advices-page/advices-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advices-page',
+  template: `
+    <p>
+      advices-page works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AdvicesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { AuctionModule } from './auction/auction.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AdviceModule } from './advice/advice.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AuctionModule
+    AuctionModule,
+    AdviceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
