d70ec0bedcfbac26845463fb3f75011badbaedb6
dudexior <dawidson1996@gmail.com>
2020-10-30 13:06:21 +0100

    dodano obsługę errorów HTTP

--- src/app/auction/auctions-page/auctions-page.component.ts
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Auction } from '../auction';
 import { AuctionsService } from '../auctions.service';
@@ -7,6 +8,9 @@ import { AuctionsService } from '../auctions.service';
   template: `
     <section>
       <h2> Lista naszych aukcji </h2>
+      <div class="alert alert-danger">
+        {{errorText}}
+      </div>
       <div class="row">
         <div *ngFor="let auction of auctions" class="col-12 col-sm-6 col-md-3 col-lg-3">
           <app-auction-item [auction]="auction"></app-auction-item>
@@ -21,11 +25,16 @@ export class AuctionsPageComponent implements OnInit {
 
   auctions: Auction[] = [];
 
+  errorText: string;
+
   constructor(private auctionsService: AuctionsService) { }
 
   ngOnInit(): void {
     this.auctionsService.getAuctions().subscribe((res: Auction[]) => {
       this.auctions = res;
+    }, (err: HttpErrorResponse) => {
+      this.errorText = err.message;
+      console.error(err);
     });
   }
 
