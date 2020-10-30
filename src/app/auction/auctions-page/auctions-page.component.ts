46c7fb9ea450e82fe3c23dbba198efa3746b0bd1
dudexior <dawidson1996@gmail.com>
2020-10-30 13:12:29 +0100

    dodano unsubscribe

--- src/app/auction/auctions-page/auctions-page.component.ts
+++ src/app/auction/auctions-page/auctions-page.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Auction } from '../auction';
 import { AuctionsService } from '../auctions.service';
 
@@ -8,7 +9,7 @@ import { AuctionsService } from '../auctions.service';
   template: `
     <section>
       <h2> Lista naszych aukcji </h2>
-      <div class="alert alert-danger">
+      <div class="alert alert-danger" *ngIf="errorText">
         {{errorText}}
       </div>
       <div class="row">
@@ -21,21 +22,26 @@ import { AuctionsService } from '../auctions.service';
   styles: [
   ]
 })
-export class AuctionsPageComponent implements OnInit {
+export class AuctionsPageComponent implements OnInit, OnDestroy {
 
   auctions: Auction[] = [];
-
   errorText: string;
+
+  auctionSubscription: Subscription;
 
   constructor(private auctionsService: AuctionsService) { }
 
   ngOnInit(): void {
-    this.auctionsService.getAuctions().subscribe((res: Auction[]) => {
+    this.auctionSubscription = this.auctionsService.getAuctions().subscribe((res: Auction[]) => {
       this.auctions = res;
     }, (err: HttpErrorResponse) => {
       this.errorText = err.message;
       console.error(err);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.auctionSubscription.unsubscribe();
   }
 
 }
